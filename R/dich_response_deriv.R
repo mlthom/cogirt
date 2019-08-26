84776cd8271268a6d05fc549df5622decece450d
Michael L. Thomas <michael.l.thomas@colostate.edu>
2019-08-26 13:41:37 -0600

    Updated naming and documentation.

---
+++ R/dich_response_deriv.R
@@ -0,0 +1,203 @@
+#-------------------------------------------------------------------------------
+#' Derivatives and information for the Dichotomous Response Model Based on
+#' Posterior Inference
+#'
+#' This function calculates the matrix of first partial dervitives, matrix of
+#' second partial dervitives, and information matrix for the posterior
+#' distribution with respect to theta. The formulas are based on Segall (1996;
+#' 2009).
+#'
+#' @references
+#'
+#' Segall, D. O. (1996). Multidimensional adaptive testing.
+#' \emph{Psychometrika, 61(2)}, 331-354. https://doi.org/10.1007/BF02294343
+#'
+#' Segall, D. O. (2009). Principles of Multidimensional Adaptive Testing. In W.
+#' J. van der Linden & C. A. W. Glas (Eds.), \emph{Elements of Adaptive Testing}
+#'  (pp. 57-75). https://doi.org/10.1007/978-0-387-85461-8_3
+#'
+#' @examples
+#' dich_response_deriv(y = sdirt$y, nu = sdirt$nu, lambda = sdirt$lambda,
+#'                 gamma = sdirt$gamma, omega = sdirt$omega, zeta = sdirt$zeta,
+#'                 omega_mu = sdirt$omega_mu, omega_sigma2 = sdirt$omega_sigma2,
+#'                 zeta_mu = sdirt$zeta_mu, zeta_sigma2 = sdirt$zeta_sigma2)
+#'
+#' @section A Note About Model Notation:
+#' The function converts GLLVM notation to the more typical IRT notation used by
+#' Segall (1996) for ease of referencing formulas.
+
+#' @export dich_response_deriv
+#' @export test_dervis_function
+#' @export test_derivs
+#-------------------------------------------------------------------------------
+
+dich_response_deriv <- function(y, nu, lambda, gamma, omega, zeta, omega_mu,
+                                omega_sigma2, zeta_mu, zeta_sigma2, link  = 'logit') {
+  a <- cbind(lambda %*% gamma, lambda)
+  ro <- nrow(omega_sigma2)
+  rz <- nrow(zeta_sigma2)
+  ra <- nrow(a)
+  ca <- ncol(a)
+  sigma2 <- diag(x = 0, nrow = ro + rz)
+  sigma2[1:ro, 1:ro] <- omega_sigma2
+  sigma2[(1 + ro):(ro + rz), (1 + ro):(ro + rz)] <- zeta_sigma2
+  rs <- nrow(sigma2)
+  theta <- cbind(omega, zeta)
+  mu <- c(omega_mu, zeta_mu)
+  mod <- dich_response_model(y = y, nu = nu, lambda = lambda, gamma = gamma,
+                             omega = omega, zeta = zeta, link  = 'logit')
+  p <- mod$p
+  # Note that this is the OPPOSITE of the simulation. I.e., if the variance of
+  # the latent response variate was 1.702 (logit) in the simualtion, then no
+  # adjustment is needed for a logit model (i.e., D = 1.000). Conversely, if the
+  # varaince was 1.000 (probit), than a 1.702 adjustment is needed.
+  D <- if(link == 'logit') {
+    1.000
+  } else if(link == 'probit'){
+    1.702
+  }
+  # Segall (1996) Equation 25; Segall (2009) Appendix
+  fpd <- list()
+  for(i in 1:nrow(theta)){
+    fpd[[i]] <- t(
+      (
+        D * apply(X = (
+          a * matrix(data = (p[i, ] * (y[i, ] - p[i, ])),
+                     nrow = ra,
+                     ncol = ca,
+                     byrow = F)
+        ) /
+          matrix(data = p[i, ],
+                 nrow = ra,
+                 ncol = ca,
+                 byrow = F),
+        MARGIN = 2,
+        FUN = sum)
+      ) - solve(sigma2) %*% t(theta[i, , drop = F] - mu)
+    )
+  }
+  # Segall (1996) Equations 30 & 31; Segall (2009) Appendix
+  spd <- list()
+  for(i in 1:nrow(theta)){
+    spd[[i]] <- matrix(data = NA,
+                       nrow = ncol(theta[i, , drop = F]),
+                       ncol = ncol(theta[i, , drop = F]))
+    diag(spd[[i]]) <-  D^2 *
+      apply(X =
+              (
+                a^2 *
+                  matrix(data = (1 - p[i, ]) * (p[i, ] * (-p[i, ]^2)),
+                         nrow = nrow(a),
+                         ncol = ncol(a),
+                         byrow = F)
+              )/
+              matrix(data = p[i, ]^2,
+                     nrow = nrow(a),
+                     ncol = ncol(a),
+                     byrow = F),
+            MARGIN = 2,
+            FUN = sum) - diag(solve(sigma2))
+    spd[[i]][lower.tri(spd[[i]])] <-
+      D^2 *
+      apply(X = (
+        apply(X = a, MARGIN = 1, FUN = prod) *
+          matrix(data = (1-p[i, ]) * (p[i, ] * (-p[i, ]^2)),
+                 nrow = nrow(a),
+                 ncol = 1,
+                 byrow = F)
+      ) /
+        matrix(p[i, ]^2,
+               nrow = nrow(a),
+               ncol = 1,
+               byrow = F),
+      MARGIN = 2,
+      FUN = sum) -
+      solve(sigma2)[lower.tri(solve(sigma2))]
+    spd[[i]][upper.tri(spd[[i]])] <- spd[[i]][lower.tri(spd[[i]])]
+  }
+  # Segall (2009) Appendix Equations 3.13
+  post_info <- list()
+  for(i in 1:nrow(theta)){
+    post_info[[i]] <-
+      solve(sigma2) +
+      apply(X = D^2 *
+              array(data = apply(X = a,
+                                 MARGIN = 1,
+                                 FUN = function(x){x %*% t(x)}),
+                    dim = c(rs, rs, nrow(a))) *
+              array(data = sapply(X = (1 - p) / p * p^2,
+                                  FUN = rep,
+                                  times = rs*rs),
+                    dim = c(rs, rs, nrow(a))),
+            MARGIN = c(1, 2),
+            FUN = sum)
+  }
+  return(list(fpd = fpd, spd = spd, post_info = post_info,
+              fisher_info = lapply(X = post_info, FUN = function(x){x * -1})))
+}
+
+# Should be deleted before realease. Used for testing.
+test_dervis_function <- function(y, nu, lambda, gamma, omega, zeta, omega_mu,
+                                 omega_sigma2, zeta_mu, zeta_sigma2, link  = 'logit'){
+  # First create a sum likelihood function that depends only on theta (combined omega and zeta)
+  # Then use my function
+  #then compare with numDeriv
+  a <- cbind(lambda %*% gamma, lambda)
+  ro <- nrow(omega_sigma2)
+  rz <- nrow(zeta_sigma2)
+  ra <- nrow(a)
+  ca <- ncol(a)
+  sigma2 <- diag(x = 0, nrow = ro + rz)
+  sigma2[1:ro, 1:ro] <- omega_sigma2
+  sigma2[(1 + ro):(ro + rz), (1 + ro):(ro + rz)] <- zeta_sigma2
+  rs <- nrow(sigma2)
+  theta <- cbind(omega, zeta)
+  mu <- c(omega_mu, zeta_mu)
+  mod <- dich_response_model(y = y, nu = nu, lambda = lambda, gamma = gamma,
+                             omega = omega, zeta = zeta, link  = 'logit')
+  p <- mod$p
+  tmp_ll <- function(theta, y, nu, a) {
+    yhatstar <- nu + theta %*% t(a)
+    p <- if(link == 'logit') {
+      plogis(yhatstar)
+    } else if(link == 'probit'){
+      pnorm(yhatstar)
+    }
+    ll <- sum(log((p^y) * (1 - p)^(1 - y)))
+    return(ll)
+  }
+  fpd <- list()
+  tmp_ll(theta[1, ], y[1, ], nu[1, ], a)
+  for(i in 1:nrow(theta)){
+    fpd[[i]] <- numDeriv::jacobian(func = tmp_ll, theta[i, ], y=y[i, ], nu=nu[i, ], a=a)
+  }
+  tmp_ll(theta[1, ], y[1, ], nu[1, ], a)
+  for(i in 1:nrow(theta)){
+    spd[[i]] <- numDeriv::hessian(func = tmp_ll, theta[i, ], y=y[i, ], nu=nu[i, ], a=a)
+  }
+  return(list(fpd = fpd, spd = spd))
+}
+
+
+test_derivs <- function(index, der_type){
+  tmp1 <- dich_response_deriv(y = sdirt$y, nu = sdirt$nu, lambda = sdirt$lambda,
+                              gamma = sdirt$gamma, omega = sdirt$omega,
+                              zeta = sdirt$zeta, omega_mu = sdirt$omega_mu,
+                              omega_sigma2 = diag(10000, 4),
+                              zeta_mu = sdirt$zeta_mu,
+                              zeta_sigma2 = diag(10000, 10))
+
+  tmp2 <- test_dervis_function(y = sdirt$y, nu = sdirt$nu, lambda = sdirt$lambda,
+                               gamma = sdirt$gamma, omega = sdirt$omega,
+                               zeta = sdirt$zeta, omega_mu = sdirt$omega_mu,
+                               omega_sigma2 = diag(10000, 4),
+                               zeta_mu = sdirt$zeta_mu,
+                               zeta_sigma2 = diag(10000, 10))
+
+  p.plot <- plot(
+    unlist(lapply(1:50,function(x){tmp1$der_type[[x]][index]})),
+    unlist(lapply(1:50,function(x){tmp2$der_type[[x]][index]}))
+  )
+  abline(a=0, b=1)
+  return(list(p.plot, tmp1$der_type[[index]], tmp2$der_type[[index]]))
+}
--- R/dich_response_model.R
+++ R/dich_response_model.R
@@ -1,30 +1,49 @@
+#-------------------------------------------------------------------------------
 #' Dichotomous Response Model
 #'
-#' This function produces predictions and log-likelihood values for a dichotmous
-#' response model.
+#' This function calculates predictions and log-likelihood values for a
+#' dichotomous response model framed using generalized latent variable modeling
+#' (GLVM; Skrondal & Rabe-Hesketh, 2004).
 #'
-#' @param y Matrix of item responses (I * J by N).
-#' @param nu Matrix of item intercept parameters (I * J by N).
+#' @param y Matrix of item responses (N by I * J).
+#' @param nu Matrix of item intercept parameters (N by I * J).
 #' @param lambda Matrix of item structure parameters (I * J by J * M).
 #' @param gamma Matrix of experimental structure parameters (J * M by K * M).
-#' @param omega Subject-level effects of the experimental manipulation (K * M by N).
-#' @param zeta Condition-level prediction errors (N by J * M).
+#' @param omega Examinee-level effects of the experimental manipulation
+#' (N by K * M).
+#' @param zeta Condition-level effects nested within examineess (N by J * M).
 #' @param link Choose between logit or probit link functions.
-#' @return yhat = response probability; yhatstar = latent response probability;
-#' loglikelihood = model log-likelihood.
+#'
+#' @return p = response probability matrix (N by I * J); yhatstar = latent
+#' response variate matrix (N by I * J); loglikelihood = model log-likelihood
+#' (scalar).
+#'
+#' @section Dimensions:
+#' N = Number of examinees; I = Number of items per condition; J = Number of
+#' conditions; M = Number of ability (or trait) dimensions; K = Number of
+#' contrasts.
+#'
+#' @references
+#'
+#' Skrondal, A., & Rabe-Hesketh, S. (2004). \emph{Generalized latent variable
+#' modeling: Multilevel, longitudinal, and structural equation models}. Boca
+#' Raton: Chapman & Hall/CRC.
+#'
 #' @examples
-#' mod <- dich_response(y = sdirt$y, nu = sdirt$nu, lambda = sdirt$lambda,
+#' mod <- dich_response_model(y = sdirt$y, nu = sdirt$nu, lambda = sdirt$lambda,
 #'                      gamma = sdirt$gamma, omega = sdirt$omega, sdirt$zeta)
-#' @export
+#'
+#' @export dich_response_model
+#-------------------------------------------------------------------------------
 
-dich_response <- function(y, nu, lambda, gamma, omega, zeta, link  = 'logit') {
-  yhatstar <- t(nu + lambda %*% gamma %*% t(omega) + lambda %*% t(zeta))
-  yhat <- if(link == 'logit') {
+dich_response_model <- function(y, nu, lambda, gamma, omega, zeta, link  = 'logit') {
+  yhatstar <- nu + omega %*% t(gamma) %*% t(lambda) + zeta %*% t(lambda)
+  p <- if(link == 'logit') {
     plogis(yhatstar)
   } else if(link == 'probit'){
     pnorm(yhatstar)
   }
-  ll <- sum(log((yhat^y) * (1 - yhat)^(1 - y)))
-  mod <- list(yhat = yhat, yhatstar = yhatstar, loglikelihood = ll)
+  ll <- sum(log((p^y) * (1 - p)^(1 - y)))
+  mod <- list(p = p, yhatstar = yhatstar, loglikelihood = ll)
   return(mod)
 }
