ec7679970c61015b5a224bb7f2926e3f84252792
Michael Thomas <mlthom@THOMAS20-2.local>
2024-11-01 17:02:39 -0600

    Add required packages.

---
+++ R/CogIRT-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics abline
+#' @importFrom graphics axis
+#' @importFrom graphics legend
+#' @importFrom graphics lines
+#' @importFrom graphics par
+#' @importFrom graphics points
+#' @importFrom grDevices n2mfrow
+#' @importFrom grDevices rgb
+#' @importFrom stats binomial
+#' @importFrom stats cor
+#' @importFrom stats glm
+#' @importFrom stats median
+#' @importFrom stats pchisq
+#' @importFrom stats plogis
+#' @importFrom stats pnorm
+#' @importFrom stats qlogis
+#' @importFrom stats qnorm
+#' @importFrom stats runif
+#' @importFrom stats sd
+#' @importFrom stats var
+## usethis namespace: end
+NULL
