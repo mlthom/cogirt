0a4dc546127209983799b13d9adf39bdf8f888a4
mlthom <michael.l.thomas@colostate.edu>
2025-10-16 15:13:32 -0600

    Add omega_mu and omega_sigma2 to output of cog_irt.

--- R/cog_irt.R
+++ R/cog_irt.R
@@ -41,8 +41,7 @@
 #' and/or lambda1), information values for all parameters estimated
 #' (info1_omega, info1_nu, and/or info1_lambda), the model log-likelihood value
 #' (log_lik), and the total number of estimated parameters (par) in the model.
-#' gamma0,  zeta0, kappa0, and and link are also returned for the purpose of
-#' computing model fit statistics.
+#' Other outputs are returned for the purpose of computing model fit statistics.
 #'
 #' @references
 #' Embretson S. E., & Reise S. P. (2000). \emph{Item response theory for
@@ -535,6 +534,8 @@ cog_irt <- function(data = NULL, model = NULL, guessing = NULL,
       "gamma0" = gamma0,
       "zeta0" = zeta0,
       "kappa0" = kappa0,
+      "omega_mu" = omega_mu,
+      "omega_sigma2" = omega_sigma2,
       "link" = link,
       "log_lik" = tmp_res$log_lik,
       "y" = y,
