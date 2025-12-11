e70d76d4cf4865b886d67d96b749c2577eaa1e90
mlthom <michael.l.thomas@colostate.edu>
2025-12-10 23:01:03 -0700

    Create new fit function.

---
+++ R/fit.R
@@ -0,0 +1,213 @@
+#-------------------------------------------------------------------------------
+#' Item fit
+#'
+#' Computes Yen's Q3 (residual correlations) and associated posterior predictive
+#' p-values (PPP) computed via parametric replication.
+#
+#' @param object An object of class 'cog_irt'.
+#' @param num_ppc Number of posterior predictive replications. Default 200.
+#
+#' @return A list with elements for fit information q3, q3_avg, q3_max,
+#' q3_avg_ppp, q3_max_ppp, q3_pair_ppp
+#
+#' @references:
+#' Yen, W. M. (1984). Effects of local item dependence on the fit and equating
+#' performance of the three-parameter logistic model. \emph{Applied
+#' Psychological Measurement, 8}, 125-145.
+#'
+#' PPP reference see Longitudinalitemresponsemodelingandposterior predictivecheckinginRandStan
+#' @export fit
+#-------------------------------------------------------------------------------
+
+fit <- function(object, ...) {
+
+  object <- fit2pcogirt #CHANGE!!!!!!!!!!!!!!!
+
+  ellipsis <- list(...)
+  #moreve this next line later
+  ellipsis <- NULL
+  if (is.null(x = ellipsis$chains)) {
+    chains <- 3
+  } else {
+    chains <- ellipsis$chains
+  }
+  if (is.null(x = ellipsis$burn)) {
+    burn <- 100
+  } else {
+    burn <- ellipsis$burn
+  }
+  if (is.null(x = ellipsis$thin)) {
+    thin <- 10
+  } else {
+    thin <- ellipsis$thin
+  }
+  if (is.null(x = ellipsis$min_tune)) {
+    min_tune <- 25
+  } else {
+    min_tune <- ellipsis$min_tune
+  }
+  if (is.null(x = ellipsis$tune_int)) {
+    tune_int <- 25
+  } else {
+    tune_int <- ellipsis$tune_int
+  }
+  if (is.null(x = ellipsis$max_tune)) {
+    max_tune <- 100
+  } else {
+    max_tune <- ellipsis$max_tune
+  }
+  if (is.null(x = ellipsis$niter)) {
+    niter <- 1100
+  } else {
+    niter <- ellipsis$niter
+  }
+
+  draws_per_chain <- ((niter - burn) / thin)
+  total_draws <- chains * draws_per_chain
+
+  # First replicate data sets for ppp
+  mc_draws <- mhmc_mc(
+    chains = chains, y = object$y, obj_fun = dich_response_model, link = object$link,
+    est_omega = object$est_omega, est_lambda = object$est_lambda, est_zeta = object$est_zeta,
+    est_nu = object$est_nu, omega0 = object$omega1, gamma0 = object$gamma0, lambda0 = object$lambda1,
+    zeta0 = object$zeta0, nu0 = object$nu1, kappa0 = object$kappa0, omega_mu = object$omega_mu,
+    omega_sigma2 = object$omega_sigma2, lambda_mu = object$lambda_mu,
+    lambda_sigma2 = object$lambda_sigma2, zeta_mu = object$zeta_mu,
+    zeta_sigma2 = object$zeta_sigma2, nu_mu = object$nu_mu, nu_sigma2 = object$nu_sigma2,
+    burn = burn, thin = thin, min_tune = min_tune, tune_int = tune_int,
+    max_tune = max_tune, niter = niter, psrf = TRUE
+  )
+
+  rep_p <- vector("list", length = total_draws)
+  for (chain in 1:chains) {
+    for(draw in 1:((niter - burn) / thin)) {
+      rep_p[[ (chain - 1) * draws_per_chain + draw]] <- dich_response_model(
+        y = object$y,
+        omega = if (object$est_omega) {
+          matrix(
+            data = mc_draws$mhmcdraws[[chain]]$omega_draws[draw, , ],
+            nrow = nrow(object$omega1),
+            ncol = ncol(object$omega1),
+            byrow = TRUE
+          )
+        } else {
+          object$omega1
+        },
+        gamma = object$gamma0,
+        lambda = if (object$est_lambda) {
+          matrix(
+            data = mc_draws$mhmcdraws[[chain]]$lambda_draws[draw, , ],
+            nrow = nrow(object$lambda1),
+            ncol = ncol(object$lambda1),
+            byrow = TRUE
+          )
+        } else {
+          object$lambda1
+        },
+        zeta = if (object$est_zeta) {
+          matrix(
+            data = mc_draws$mhmcdraws[[chain]]$zeta_draws[draw, , ],
+            nrow = nrow(object$zeta0),
+            ncol = ncol(object$zeta0),
+            byrow = TRUE
+          )
+        } else {
+          object$zeta0
+        },
+        nu = if (object$est_nu) {
+          matrix(
+            data = mc_draws$mhmcdraws[[chain]]$nu_draws[draw, , ],
+            nrow = nrow(object$nu1),
+            ncol = ncol(object$nu1),
+            byrow = TRUE
+          )
+        } else {
+          object$nu1
+        },
+        kappa = object$kappa0,
+        link = object$link
+        )$p
+    }
+  }
+
+
+  # Compute item fit based on observed data
+  p <- dich_response_model(y = object$y, omega = object$omega1, gamma = object$gamma0,
+                            lambda = object$lambda1, zeta = object$zeta0, nu = object$nu1,
+                            kappa = object$kappa0, link = object$link)$p
+
+  # Q3 Statistic observed ------------------------------------------------------
+  q3_func <- function(y, p){
+    r  <- y - p
+    #rm <- rowMeans(r, na.rm = TRUE)
+    #rc <- r - rm
+    #v  <- apply(rc, 2, var, na.rm = TRUE)
+    #keep <- is.finite(v) & v > 0
+    rmat <- stats::cor(r, use = "pairwise.complete.obs")
+    #if (any(keep)) {
+    #  rk <- stats::cor(rc[, keep, drop = FALSE], use = "pairwise.complete.obs")
+    #  rmat[keep, keep] <- rk
+    #}
+    diag(rmat) <- NA_real_
+    rmat
+  }
+  q3 <- q3_func(object$y, p)
+
+  # Q3 Statistic ppp ------------------------------------------------------
+
+  rep_q3 <- vector("list", length = total_draws)
+  for (i in 1:total_draws) {
+    rep_q3[[i]] <- q3_func(object$y, rep_p[[i]])
+  }
+
+  # Q3 discrepency ------------------------------------------------------
+  ppp_q3 <- matrix(NA, ncol(object$y), ncol(object$y))
+  for (i in 1:ncol(object$y)) {
+    for (ii in 1:ncol(object$y)) {
+      ppp_q3[i, ii] <- mean(q3[i, ii] > vapply(rep_q3, function(m) m[i, ii], numeric(1)))
+    }
+  }
+
+
+  list(
+    q3 = q3,
+    ppp_q3 = ppp_q3
+  )
+
+#   # Posterior predictive p-values ----------------------------------------------
+#   q3_avg_rep <- numeric(0)
+#   q3_max_rep <- numeric(0)
+#   q3_pair_ppp <- NULL
+#
+#   for (b in seq_len(num_ppc)) {
+#     Yrep <- matrix(stats::rbinom(N * I, 1, prob = as.vector(P)), nrow = N)
+#     q3r <- q3_func(Yrep, P)
+#     q3_avg_rep <- c(q3_avg_rep, mean(q3r, na.rm = TRUE))
+#     q3_max_rep <- c(q3_max_rep, max(q3r,  na.rm = TRUE))
+#   }
+#
+#   q3_avg_ppp <- mean(q3_avg_rep >= q3_avg, na.rm = TRUE)
+#   q3_max_ppp <- mean(q3_max_rep >= q3_max, na.rm = TRUE)
+#
+#   lowtri_idx <- which(lower.tri(q3))
+#   rep_mat <- matrix(NA_real_, num_ppc, length(lowtri_idx))
+#   for (b in seq_len(num_ppc)) {
+#     Yrep <- matrix(stats::rbinom(N * I, 1, as.vector(P)), nrow = N)
+#     q3r <- q3_func(Yrep, P)
+#     rep_mat[b, ] <- q3r[lowtri_idx]
+#   }
+#   vobs <- q3[lowtri_idx]
+#   pair_ppp <- colMeans(t(t(rep_mat) >= vobs), na.rm = TRUE)
+#   q3_pair_ppp <- matrix(NA_real_, I, I)
+#   diag(q3_pair_ppp) <- NA_real_
+#   q3_pair_ppp[lowtri_idx] <- pair_ppp
+#
+#   list(
+#     q3 = q3,
+#     q3_avg = q3_avg,
+#     q3_max = q3_max,
+#     q3_avg_ppp = q3_avg_ppp,
+#     q3_max_ppp = q3_max_ppp,
+#     q3_pair_ppp = q3_pair_ppp
+#   )
+}
